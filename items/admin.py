d1173bc84137775c8a6230c2e07ce78dcf6b72da
BERiG75 <berdinigor25@gmail.com>
2026-07-25 15:27:35 +0700

    Register Item in Django Admin

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Item
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "owner",
+        "created_at",
+        "updated_at",
+    )
+
+    list_display_links = (
+        "name",
+    )
+
+    list_select_related = (
+        "owner",
+    )
+
+    ordering = (
+        "-created_at",
+        "-id",
+    )
+
+    list_per_page = 50
+    
