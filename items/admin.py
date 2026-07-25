4a5f2228d480da2ccb629f7a6152a2875d2f4987
BERiG75 <berdinigor25@gmail.com>
2026-07-25 15:43:27 +0700

    Restrict Item access by owner

--- items/admin.py
+++ items/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db.models import QuerySet
+from django.http import HttpRequest
 
 from .models import Item
 
@@ -27,4 +29,39 @@ class ItemAdmin(admin.ModelAdmin):
     )
 
     list_per_page = 50
-    
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Item]:
+        queryset = super().get_queryset(request)
+
+        if request.user.is_superuser:
+            return queryset
+
+        return queryset.filter(owner=request.user)
+
+    def get_readonly_fields(
+        self,
+        request: HttpRequest,
+        obj: Item | None = None,
+    ) -> tuple[str, ...]:
+        if request.user.is_superuser:
+            return ()
+
+        return ("owner",)
+
+
+    def save_model(
+        self,
+        request: HttpRequest,
+        obj: Item,
+        form,
+        change: bool,
+    ) -> None:
+        if not change and not request.user.is_superuser:
+            obj.owner = request.user
+
+        super().save_model(
+            request,
+            obj,
+            form,
+            change,
+        )
