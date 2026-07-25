54ee577f4bc4005e437b763e94c42b39b298e14e
BERiG75 <berdinigor25@gmail.com>
2026-07-25 15:01:16 +0700

    Create Item model

--- items/models.py
+++ items/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Item(models.Model):
+    name = models.CharField(
+        verbose_name="Name",
+        max_length=255,
+    )
+    description = models.TextField(
+        verbose_name="Description",
+        blank=True,
+    )
+    owner = models.ForeignKey(
+        User,
+        verbose_name="Owner",
+        on_delete=models.CASCADE,
+        related_name="items",
+    )
+    created_at = models.DateTimeField(
+        verbose_name="Created at",
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        verbose_name="Updated at",
+        auto_now=True,
+    )
+
+    class Meta:
+        db_table = "items"
+        ordering = ("id",)
+        permissions = (
+            (
+                "export_items_csv",
+                "Can export items to CSV",
+            ),
+        )
+        verbose_name = "Item"
+        verbose_name_plural = "Items"
+
+    def __str__(self) -> str:
+        return self.name
