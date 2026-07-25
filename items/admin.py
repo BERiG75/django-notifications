b1056fcd4e8d30253f13e4b6748b80d3328fbac6
BERiG75 <berdinigor25@gmail.com>
2026-07-25 18:53:18 +0700

    Add download to CSV button

--- items/admin.py
+++ items/admin.py
@@ -1,6 +1,10 @@
+import csv
+
 from django.contrib import admin
+from django.core.exceptions import PermissionDenied
 from django.db.models import QuerySet
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
+from django.urls import path
 
 from .filters import NameFilter, UsernameFilter
 from .models import Item
@@ -14,6 +18,10 @@ class ItemAdmin(admin.ModelAdmin):
         "owner",
         "created_at",
         "updated_at",
+    )
+
+    change_list_template = (
+        "admin/items/item/change_list.html"
     )
 
     list_display_links = (
@@ -30,7 +38,7 @@ class ItemAdmin(admin.ModelAdmin):
         UsernameFilter,
         NameFilter,
     )
-    
+
     ordering = (
         "-created_at",
         "-id",
@@ -56,7 +64,6 @@ class ItemAdmin(admin.ModelAdmin):
 
         return ("owner",)
 
-
     def save_model(
         self,
         request: HttpRequest,
@@ -73,3 +80,96 @@ class ItemAdmin(admin.ModelAdmin):
             form,
             change,
         )
+
+    def get_urls(self):
+        urls = super().get_urls()
+
+        custom_urls = [
+            path(
+                "export-csv/",
+                self.admin_site.admin_view(self.export_csv),
+                name="items_item_export_csv",
+            ),
+        ]
+
+        return custom_urls + urls
+
+    def export_csv(self, request: HttpRequest) -> HttpResponse:
+        if not request.user.has_perm("items.export_items_csv"):
+            raise PermissionDenied
+
+        queryset = (
+            self.get_queryset(request)
+            .select_related("owner")
+        )
+
+        response = HttpResponse(
+            content_type="text/csv; charset=utf-8",
+        )
+        response["Content-Disposition"] = (
+            'attachment; filename="items.csv"'
+        )
+
+        response.write("\ufeff")
+
+        writer = csv.writer(
+            response,
+            delimiter=";",
+            lineterminator="\r\n",
+        )
+
+        writer.writerow(
+            [
+                "id",
+                "name",
+                "description",
+                "username",
+                "created_at",
+                "updated_at",
+            ]
+        )
+
+        for item in queryset.iterator():
+            writer.writerow(
+                [
+                    item.pk,
+                    item.name,
+                    item.description,
+                    item.owner.username,
+                    item.created_at.isoformat(),
+                    item.updated_at.isoformat(),
+                ]
+            )
+
+        return response
+
+    # def has_export_csv_permission(
+    #     self,
+    #     request: HttpRequest,
+    # ) -> bool:
+    #     opts = self.model._meta
+    #     codename = get_permission_codename(
+    #         "export_items_csv",
+    #         opts,
+    #     )
+
+    #     return request.user.has_perm(
+    #         f"{opts.app_label}.{codename}"
+    #     )
+
+    # def changelist_view(
+    #     self,
+    #     request: HttpRequest,
+    #     extra_context=None,
+    # ):
+    #     if extra_context is None:
+    #         extra_context = {}
+
+    #     extra_context["has_export_csv_permission"] = (
+    #         self.has_export_csv_permission(request)
+    #     )
+
+    #     return super().changelist_view(
+    #         request,
+    #         extra_context=extra_context,
+    #     )
